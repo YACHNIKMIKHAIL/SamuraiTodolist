92f563f94c2b052b35460d0bf7511ce92bf1f82a
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-15 14:05:13 +0300

    removeTask commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import styled from "styled-components";
 
 function App() {
 
-    const tasks1 = [
+    const initTasks = [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false}
+        {id: 3, title: "ReactJS", isDone: false},
+        {id: 4, title: "Hello world", isDone: true},
+        {id: 5, title: "I am Happy", isDone: false},
+        {id: 6, title: "Yo", isDone: false}
     ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ]
+    const [tasks,setTasks]=useState(initTasks)
+
+    const removeTask=(id:number)=>{
+        setTasks(tasks.filter(f=>f.id!==id))
+    }
 
     return (
         <AppCase>
-            <Todolist title="What to learn" tasks={tasks1}/>
-            <Todolist title="Songs" tasks={tasks2}/>
+            <Todolist title="What to learn" tasks={tasks} removeTask={removeTask}/>
         </AppCase>
     );
 }
