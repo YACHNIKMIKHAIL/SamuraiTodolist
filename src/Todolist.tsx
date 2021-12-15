8bf95fb97ee942feab1c6e6b195595244f9d1272
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-15 14:14:32 +0300

    changeFilter commit

---
+++ src/Todolist.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export type FilterType='all'|'complited'|'active'
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask:(id:number)=>void
+    changeFilter:(filter:FilterType)=>void
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {props.tasks.map(m=><li key={m.id}>
+                <input type="checkbox" checked={m.isDone}/>
+                <span>{m.title}</span>
+                <button onClick={()=>props.removeTask(m.id)}>x</button>
+            </li>
+            )}
+
+
+        </ul>
+        <div>
+            <button onClick={()=>props.changeFilter('all')}>All</button>
+            <button onClick={()=>props.changeFilter('active')}>Active</button>
+            <button onClick={()=>props.changeFilter('complited')}>Completed</button>
+        </div>
+    </div>
+}
