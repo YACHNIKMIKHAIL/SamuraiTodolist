c919d27972914caf562d2e7c3922e6935d01c17f
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-15 13:42:49 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import styled from "styled-components";
+
+function App() {
+    return (
+        <AppCase>
+
+        </AppCase>
+    );
+}
+
+export default App;
+
+const AppCase = styled.div`
+  height: 100vh;
+  overflow: auto;
+  //background: url("./Components/Images/wallpaperflare.com_wallpaper (9).jpg") no-repeat center/cover;
+`
