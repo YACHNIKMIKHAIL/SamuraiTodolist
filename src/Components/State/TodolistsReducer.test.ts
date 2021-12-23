de52c3eb0ab981a5fa8ceb677b0fc4405f52af7a
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-23 14:01:43 +0300

    removeTodo reducer)  commit

--- src/Components/State/TodolistsReducer.test.ts
+++ src/Components/State/TodolistsReducer.test.ts
@@ -1,6 +1,6 @@
 import {TodolistType} from "../../Todolist";
 import {v1} from "uuid";
-import {changeTodoFilter, TodolistsReducer} from "./TodolistsReducer";
+import {changeTodoFilterAC, removeTodoAC, TodolistsReducer} from "./TodolistsReducer";
 
 test('change todo filter',()=>{
     const todolist1 = v1()
@@ -17,8 +17,52 @@ test('change todo filter',()=>{
         {id: todolist5, title: "Что пить?", filter: 'all'}
     ]
 
-    const result=TodolistsReducer(initTodolists,changeTodoFilter(todolist2,'active'))
+    const result=TodolistsReducer(initTodolists,changeTodoFilterAC(todolist2,'active'))
 
     expect(result[1].filter).toBe('active')
     expect(result[0].filter).toBe('all')
+})
+
+test('remove todo',()=>{
+    const todolist1 = v1()
+    const todolist2 = v1()
+    const todolist3 = v1()
+    const todolist4 = v1()
+    const todolist5 = v1()
+
+    let initTodolists: TodolistType[] = [
+        {id: todolist1, title: "Что учить?", filter: 'all'},
+        {id: todolist2, title: "Что покупать?", filter: 'all'},
+        {id: todolist3, title: "Что посмотреть?", filter: 'all'},
+        {id: todolist4, title: "Что сходить?", filter: 'all'},
+        {id: todolist5, title: "Что пить?", filter: 'all'}
+    ]
+
+    const result=TodolistsReducer(initTodolists,removeTodoAC(todolist2))
+
+    expect(result.length).toBe(4)
+    expect(result[0].filter).toBe('all')
+    expect(result[1].id).toBe(todolist3)
+})
+
+test('remove todo',()=>{
+    const todolist1 = v1()
+    const todolist2 = v1()
+    const todolist3 = v1()
+    const todolist4 = v1()
+    const todolist5 = v1()
+
+    let initTodolists: TodolistType[] = [
+        {id: todolist1, title: "Что учить?", filter: 'all'},
+        {id: todolist2, title: "Что покупать?", filter: 'all'},
+        {id: todolist3, title: "Что посмотреть?", filter: 'all'},
+        {id: todolist4, title: "Что сходить?", filter: 'all'},
+        {id: todolist5, title: "Что пить?", filter: 'all'}
+    ]
+
+    const result=TodolistsReducer(initTodolists,removeTodoAC(todolist2))
+
+    expect(result.length).toBe(4)
+    expect(result[0].filter).toBe('all')
+    expect(result[1].id).toBe(todolist3)
 })
--- src/Components/State/TodolistsReducer.ts
+++ src/Components/State/TodolistsReducer.ts
@@ -5,20 +5,33 @@ export const TodolistsReducer = (state: TodolistType[], action: ActionType) => {
         case "CHANGE_FILTER": {
             return state.map(m=>m.id===action.payload.todoID?{...m,filter:action.payload.filter}:m)
         }
+        case "REMOVE_TODO": {
+            return state.filter(f=>f.id!==action.payload.todoID)
+        }
         default:
             return state
     }
 };
 
-type ActionType = changeFilterACType
+type ActionType = changeFilterACType|removeTodoACType
 
-type changeFilterACType = ReturnType<typeof changeTodoFilter>
-export const changeTodoFilter = (todoID:string,filter: FilterType) => {
+type changeFilterACType = ReturnType<typeof changeTodoFilterAC>
+export const changeTodoFilterAC = (todoID:string, filter: FilterType) => {
     return {
         type: 'CHANGE_FILTER',
         payload: {
             todoID:todoID,
             filter: filter
+        }
+    } as const
+}
+
+type removeTodoACType = ReturnType<typeof removeTodoAC>
+export const removeTodoAC = (todoID:string) => {
+    return {
+        type: 'REMOVE_TODO',
+        payload: {
+            todoID:todoID
         }
     } as const
 }
