bff1c76e3eaf286a2a2fa39593262e639b019b7a
YACHNIKMIKHAIL <limerick2011@yandex.by>
2021-12-23 14:34:06 +0300

    add Task reducer) commit

--- src/Components/State/TasksReducer.ts
+++ src/Components/State/TasksReducer.ts
@@ -1,4 +1,5 @@
 import {TasksStateType} from "../../App";
+import {v1} from "uuid";
 
 
 export const TasksReducer = (state:TasksStateType,action:ActionType) => {
@@ -6,12 +7,15 @@ switch (action.type){
     case "REMOVE_TASK":{
         return {...state,[action.payload.todoID]:state[action.payload.todoID].filter(f=>f.id!==action.payload.taskID)}
     }
+    case "ADD_TASK":{
+        return {...state,[action.payload.todoID]:[{id: v1(), title: action.payload.title, isDone: false},...state[action.payload.todoID]]}
+    }
     return state
 }
 
 };
 
-type ActionType = removeTaskACType
+type ActionType = removeTaskACType|addTaskACType
 
 type removeTaskACType = ReturnType<typeof removeTaskAC>
 export const removeTaskAC = (todoID:string, taskID: string) => {
@@ -20,6 +24,17 @@ export const removeTaskAC = (todoID:string, taskID: string) => {
         payload: {
             todoID:todoID,
             taskID:taskID
+        }
+    } as const
+}
+
+type addTaskACType = ReturnType<typeof addTaskAC>
+export const addTaskAC = (title: string,todoID:string) => {
+    return {
+        type: 'ADD_TASK',
+        payload: {
+            todoID:todoID,
+            title:title
         }
     } as const
 }
